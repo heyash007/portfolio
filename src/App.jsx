ad04e4f7789f017c9aac534d751917108d2c6324
Aayushi <heyaayushi007@gmail.com>
2026-05-26 12:48:59 +0530

    EB back on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export default function App() {
 
     const gridCards = activeFilter
         ? cards.filter((c) => c.category === activeFilter)
-        : cards.slice(0, 5)
+        : cards.slice(0, 6)
 
     const filteredCards = activeFilter
         ? cards.filter((c) => c.category === activeFilter)
